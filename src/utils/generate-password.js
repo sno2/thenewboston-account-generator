f0d3f789728e235fdbfb7fd98c72c2183eced51f
Carter Snook <cartersnook04@gmail.com>
2021-04-04 21:45:19 -0500

    fix: schema

--- src/utils/generate-password.js
+++ src/utils/generate-password.js
@@ -1,6 +1,6 @@
 export function generatePassword(length) {
   const charset =
-    "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz+!#^*()=-_~";
+    "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz:-_";
   return Array(length)
     .fill(charset)
     .map((x) => x[Math.floor(Math.random() * x.length)])
